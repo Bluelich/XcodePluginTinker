2271f5c9023c9666acaae397bcee5fbe13843252
Bluelich <bluelich@qq.com>
2016-08-06 17:21:55 +0800

    Change the language to English
    Change the language to English

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # XcodePluginTinker
 Fix Xcode plugin when you upgrade the xcode
+
+
+
+You can use it in two ways
+
+**1**.  curl -fsSL https://raw.githubusercontent.com/Bluelich/XcodePluginTinker/master/XcodePluginTinker.sh | sh
+
+**2** . clone this repo,
+
+​      `cd XcodePluginTinker`
+
+​      Drag `XcodePluginTinker.sh` to the window of your terminal,then press Enter
--- XcodePluginTinker.sh
+++ XcodePluginTinker.sh
@@ -1,23 +1,24 @@
 #!/bin/bash
-#选中默认的Xcode版本
 echo "**************************************************************"
-echo "          选择 /Applications/Xcode.app/ 为默认的Xcode           "
-echo "                 (第一次启动可能会需要输入密码)                    "
+echo "    Switch the detault Xcode to /Applications/Xcode.app/      "
+echo "      (You may need to input the password of your Mac)        "
 echo "**************************************************************"
 sudo xcode-select -switch /Applications/Xcode.app/
-#获取当前版本Xcode的DVTPlugInCompatibilityUUID
 UUID=$(defaults read /Applications/Xcode.app/Contents/Info DVTPlugInCompatibilityUUID)
-echo "读取到Xcode的UUID 为 $UUID"
-#遍历每一个Xcode插件，将UUID写入插件的兼容列表中
+echo "Xcode UUID is $UUID"
 for Plugin in ~/Library/Application\ Support/Developer/Shared/Xcode/Plug-ins/*
 do
     UUIDs=$(defaults read "$Plugin"/Contents/Info DVTPlugInCompatibilityUUIDs)
     BundleName=$(defaults read "$Plugin"/Contents/Info CFBundleName)
     if echo "${UUIDs[@]}" | grep -w "$UUID" &>/dev/null; then
-        echo "$BundleName 已经被正确设置"
+        echo "The setting of $BundleName is already done!"
     else
 #        defaults write "$Plugin"/Contents/Info DVTPlugInCompatibilityUUIDs -array-add $UUID
         echo ------------- Write UUID to $BundleName succeed!
     fi
 done
-echo "Done!"
+echo ""
+echo "**************************************************************"
+echo "                Done! Please restart Xcode!                   "
+echo "  ❡ ❡ ❡ ❡ ❡ ❡        Enjoy yourself!            ❡ ❡ ❡ ❡ ❡ ❡   "
+echo "**************************************************************"
